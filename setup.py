6a40a3f6cd646cb639a26da9ce53ad48ee4889e5
Marcus Stinghel <vini.o.stinghel@gmail.com>
2024-02-25 17:25:24 -0300

    Debugged - BOM removed

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 with open('requirements.txt', encoding='utf-16le') as file:
     requirements = file.read().splitlines()
 
+if requirements and requirements[0].startswith('\ufeff'):
+    requirements[0] = requirements[0][1:]
+
 setup(
     name='apollo_sdk',
     version='1.0',
